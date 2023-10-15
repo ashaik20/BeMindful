0956b87274f25376317a56f8b213ea0ffd60c929
mithunsubhash <mkamalapriya3@gatech.edu>
2023-10-15 00:02:05 -0400

    login

--- BeMindfulServer/controllers/user.controller.js
+++ BeMindfulServer/controllers/user.controller.js
@@ -2,6 +2,8 @@ const User = require('../models/user.model')
 const router = require('express').Router()
 
 router.route('/new').post()
+router.route('/login').post()
+
 
 // router.route('/').get()
 
@@ -18,6 +20,13 @@ router.route('/new').post((req, res)=>{
 
 })
 
+router.route('/login').post((req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+    const user = User.findOne({email: email, password: password})
+        .then(user => res.json(user))
+        .catch(err => res.status(400).json("Error! " + err))
+})
 
 
 module.exports = router
